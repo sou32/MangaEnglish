601fa5d5a3362dc24d80bcd229911ddaa256c64f
sou32 <osasou0329@gmail.com>
2026-01-17 01:35:49 +0900

    9.0に戻す

--- MangaEnglish/MauiProgram.cs
+++ MangaEnglish/MauiProgram.cs
@@ -14,7 +14,7 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+            });　
 
         builder.Services.AddSingleton<DatabaseService>(s =>
         {
